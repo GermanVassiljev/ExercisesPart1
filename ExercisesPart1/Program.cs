70bb0164c95b70c487515498d0d6a30e8b026de3
Opilane TTHK <opilane@tthk.ee>
2021-12-07 13:29:32 +0200

    Add project files.

---
+++ ExercisesPart1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercisesPart1
+{
+    class Program
+    {
+        private const int V = 2021;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name:");
+            string First_name = Console.ReadLine();
+            Console.WriteLine("Enter your last name:");
+            string Last_name = Console.ReadLine();
+            Console.WriteLine("Greetings " + First_name + " " + Last_name + "!");
+            Console.WriteLine("Print your year birth, please: ");
+            int Year_birth = Convert.ToInt32(Console.ReadLine());
+            int currentDate = DateTime.Now.Year;
+            int age = 2021 - Year_birth;
+            if (age >= 13)
+            {
+                Console.WriteLine("Welcome to Instagram!");
+            } 
+            else
+            {
+                Console.WriteLine("You are too young to have Instagram account.");
+            }
+        }
+    }
+}
---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite color: ");
+            string Color = Console.ReadLine().ToLower();
+            if (Color == "red")
+            {
+                Console.WriteLine($"{Color}. You are a romantic!");
+            }
+            else if (Color == "blue")
+            {
+                Console.WriteLine($"{Color}. You are a relible!");
+            }
+            else if (Color == "yellow")
+            {
+                Console.WriteLine($"{Color}. You are a cheerful!");
+            }
+            else
+            {
+                Console.WriteLine($"You are a {Color} unicorn!!!");
+            }
+
+        }
+    }
+}
